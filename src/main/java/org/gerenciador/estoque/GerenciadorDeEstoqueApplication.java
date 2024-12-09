b50b4061fdd915a9dd03e0647150b266ca9a3d70
aly50n <alyson.morato13@gmail.com>
2024-12-09 10:48:20 -0300

    Notação para definir a url defult pro Swagger

--- src/main/java/org/gerenciador/estoque/GerenciadorDeEstoqueApplication.java
+++ src/main/java/org/gerenciador/estoque/GerenciadorDeEstoqueApplication.java
@@ -1,9 +1,12 @@
 package org.gerenciador.estoque;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers= {@Server(url="/", description = "defult server URL")})
 public class GerenciadorDeEstoqueApplication {
 
 	public static void main(String[] args) {
