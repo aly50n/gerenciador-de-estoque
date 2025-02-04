b47a037d15e28d771097bf11f1d48a7f2d0f9f53
aly50n <alyson.morato13@gmail.com>
2025-02-04 09:23:26 -0300

    refactor: reorganização na ordem das linhas sem modificar funcionalidade"

--- src/main/java/org/gerenciador/estoque/infra/security/SecurityConfig.java
+++ src/main/java/org/gerenciador/estoque/infra/security/SecurityConfig.java
@@ -31,9 +31,9 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
